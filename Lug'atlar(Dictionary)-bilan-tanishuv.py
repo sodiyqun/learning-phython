fcb420bf0110156dd901243983c8a39af6b2faad
sodiyqun <94372756+sodiyqun@users.noreply.github.com>
2021-11-19 02:06:02 +0500

    Create Lug'atlar(Dictionary)-bilan-tanishuv.py

---
+++ Lug'atlar(Dictionary)-bilan-tanishuv.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 01:22:18 2021
+
+@author: sodiyqun
+"""
+
+# ukam = {'ism': 'Zabixulloh', 'yili': 2010, 'shahar': 'Toshkent'}
+# akam = {'ism': 'Fayzulloh', 'yili': 1999, 'shahar': 'Toshkent'}
+# dostim = {'ism': 'Yoqub', 'yili': 2003, 'shahar': 'Toshkent'}
+# print(f"Ukamning ismi {ukam['ism']}, {ukam['yili']}-yulda, {ukam['shahar']} shahrida tug'ilgan.")
+# print(f"Akamning ismi {akam['ism']}, {akam['yili']}-yulda, {akam['shahar']} shahrida tug'ilgan.")
+# print(f"Do'stimning ismi {dostim['ism']}, {dostim['yili']}-yulda, {dostim['shahar']} shahrida tug'ilgan.")
+
+
+# roll = {
+#         'ukam': 'Osh',
+#         'akam': 'Manti',
+#         'opam': 'Somsa',
+#         'otam': 'sho\'rva',
+#         'men': 'kabob'
+#         }
+# print(f"Ukamning sevimli taomi {roll['ukam'].lower()}")
+# print(f"Akamning sevimli taomi {roll['akam'].lower()}")
+# print(f"Opamning sevimli taomi {roll['opam'].lower()}")
+
+
+# pythondc = {
+#     'integer': "Butun son",
+#     'float': "O'nlik son",
+#     'string': "Matn",
+#     'list': "Ro'yxat",
+#     'tuple': "O'zgarmas ro'yxat",
+#     'if': "Agar",
+#     'else': "aks holda"
+#     }
+
+# keydc = input("Kalit so'z kiriting: ").lower()
+# print((pythondc.get(keydc, "Bungay so'z mavjud emas")).capitalize())
+
+
+# keydc = input("Kalit so'z kiriting: ").lower()
+# tarjima = pythondc.get(keydc)
+# if tarjima == None:
+#     print("Bunday so'z mavjud emas")
+# else:
+#     print(f'{keydc.title()} so\'zi "{tarjima}" deb tarjima qilinadi')
