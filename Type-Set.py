148b345b821c735e7a6bed711d16ce97a59003e1
sodiyqun <94372756+sodiyqun@users.noreply.github.com>
2021-11-20 02:31:43 +0500

    add

---
+++ Type-Set.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 05:17:59 2021
+
+@author: sodiyqun
+"""
+
+# colors = {'red', 'blue', 'green'}
+# colors.add('yellow')
+# colors.update(['black', 'white', 'grey'])
+
+
+# set1 = {10, 20, 30, 40, 50}
+# set2 = {30, 40, 50, 60, 70}
+# set3 = set1 & set2
+# print(set3)
+
+
+# print(set1-set2)
+# print(set2-set1)
+# print(set1^set2)
+
+
+s
---
+++ Working-with-Dictionary.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 04:37:59 2021
+
+@author: sodiyqun
+"""
+
+# pykeys = {
+#     'integer': "Butun son",
+#     'float': "O'nlik son",
+#     'boolean': "Mantiqiy qiymat",
+#     'for': "Biror amalni qayta-qayta bajarish tsikli",
+#     'if': "Shartlarni tekshirish operatori"
+#     }
+# for k, v in sorted(pykeys.items()):
+#     print(f"{k.title()} - {v}")
+
+
+# davlatlar = {
+#     "o'zbekiston":'toshkent',
+#     'aqsh':'washington d.c.',
+#     'rossiya':'moskva',
+#     'tojikiston':'dushanbe',
+#     "qirg'iziston":'bishkek',
+#     'qozog\'iston':'nursulton',
+#     'malayziya':'kuala-lumpur',
+#     'singapur':'sungapur',
+#     'italiya':'rim'
+#     }
+# print("Dunyo davlatlari:")
+# for c in sorted(davlatlar):
+#     if c == 'aqsh':
+#         print(c.upper())
+#     else:
+#         print(c.capitalize())
+# print("\nDavlatlarning poytaxti:")
+# for t in sorted(davlatlar.values()):
+#     print(t.title())
+
+
+# country = input("Qaysi davlatning poytaxtini bilishni istaysiz?: ").lower()
+# city = davlatlar.get(country)
+# if city == None:
+#     print("Kechirasiz, bizda bu haqida ma'lumot yo'q.")
+# elif country == "aqsh":
+#     print(f"{country.upper()}'ning poytaxti {city.title()}")
+# else:
+#     print(f"{country.capitalize()}ning poytaxti {city.title()}")
+
+
+# menu = {
+#         'osh':20000,
+#         "lag'mon":22000,
+#         'non':4000,
+#         'choy':5000,
+#         'shashlik':12000,
+#         'somsa':6000,
+#         'tabaka':15000
+#         }
+# print('3 ta taom buyurtma bering.')
+# buyurtmalar = []
+# for n in range(3):
+#     buyurtmalar.append(input(f"{n+1} - taom: "))
+# print('')
+# for taom in buyurtmalar:
+#     if taom in menu:
+#         print(f"{taom.title()} {menu[taom]} so'm")
+#     else:
+#         print(f"Kechirasiz, bizda {taom.title()} yo'q")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
