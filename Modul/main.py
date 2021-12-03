298390a506a7054f5523d8116f75af71f74e9d40
sodiyqun <94372756+sodiyqun@users.noreply.github.com>
2021-12-03 19:34:54 +0500

    add

--- Modul/main.py
+++ Modul/main.py
@@ -5,3 +5,36 @@ Created on Fri Dec  3 18:49:47 2021
 @author: sodiyqun
 """
 
+# import func_mod 
+
+# students = func_mod.info_kirit()
+# func_mod.info_chiqar(students)
+
+
+
+# import func_mod as fm
+
+# students = fm.info_kirit()
+# fm.info_chiqar(students)
+
+
+
+# from func_mod import info_kirit, info_chiqar
+
+# students = info_kirit()
+# info_chiqar(students)
+
+
+
+# from func_mod import info_kirit as ik, info_chiqar as ich
+
+# students = ik()
+# ich(students)
+
+
+
+# from func_mod import *
+'''NOT recommended'''
+
+# students = info_kirit()
+# info_chiqar(students)
